a118d9b660bcf6f2f9aec374f497d9ac33796b2e
Raoni gomes oliveira ferino <raoniferino@gmail.com>
2025-01-19 16:59:00 +0000

    Melhorando a UX do app.

--- app/src/main/java/com/example/tips_calculator/SummaryActivity.kt
+++ app/src/main/java/com/example/tips_calculator/SummaryActivity.kt
@@ -13,19 +13,19 @@ class SummaryActivity : AppCompatActivity() {
         binding = ActivitySummaryBinding.inflate(this.layoutInflater)
         setContentView(binding.root)
 
-        // Recuperando os dados do Intent com as keys corretas
+
         val totalTable = intent.getFloatExtra("totalTable", 0F)
         val numPeople = intent.getIntExtra("numPeople", 0) // Key corrigida
         val percentage = intent.getIntExtra("percentage", 0) // Key corrigida
         val totalAmount = intent.getFloatExtra("totalAmount", 0F) // Key corrigida
 
-        // Atualizando os TextViews com os valores
+
         binding.tvPercentage.text = "$percentage%"
         binding.tvTotalAmount.text = String.format("%.2f", totalAmount)
         binding.tvNumPeopleTable.text = numPeople.toString()
         binding.tvTotalTable.text = String.format("%.2f", totalTable)
 
-        // Botão para finalizar a Activity
+
         binding.btnDone.setOnClickListener {
             finish()
         }
